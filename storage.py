8ce43dd5f0fa3835321b80451d5447a139d1ecab
BBYBOI <zeygermanleo@gmail.com>
2026-05-21 18:53:25 +0500

    add storage

---
+++ storage.py
@@ -0,0 +1,12 @@
+import json
+
+def load_data():
+    try:
+        with open('data/products.json', 'r') as f:
+            return json.load(f)
+    except:
+        return []
+    
+def save_data(data):
+    with open('data/product.json', 'w') as f:
+        json.dump(data, f)
