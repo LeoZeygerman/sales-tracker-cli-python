306feaa9d34d1847450a07c4b0b34f97af941dc5
BBYBOI <zeygermanleo@gmail.com>
2026-05-21 20:08:49 +0500

    add first option

--- main.py
+++ main.py
@@ -9,5 +9,27 @@ while True:
         print('5.Удалить товар')
         print('6.Выйти')
         choice = int(input('Введите номер интересующей вас операции: '))
+        
+        if choice == 1:
+            name = input('Введите название товара: ')
+            price = int(input('Введите цену товара: '))
+            quantity = int(input('Введите количество товара: '))
+            data = load_data()
+            if len(data) == 0:
+                new_id = 1
+            else:
+                new_id = data[-1]['new_id'] + 1
+                
+            product = {
+                'new_id': new_id,
+                'name': name,
+                'price': price,
+                'quantity': quantity
+            }
+            data.append(product)
+            save_data(data)
+            product_object = Product(new_id, name, price, quantity)
+            product_object.get_info()
+        
     except ValueError:
         print('Ошибка при вводе.')
